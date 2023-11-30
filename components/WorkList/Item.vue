1ae99044167b4910289e71b43cb5021675db70da
snowsphere <info@snowsphere.net>
2023-11-30 12:08:18 +0900

    feat: 作品情報とsoundcloudの埋め込みを追加

--- components/WorkList/Item.vue
+++ components/WorkList/Item.vue
@@ -10,8 +10,55 @@ const props = defineProps<Props>()
 
 <template>
   <div class="py-4">
-    <div class="text-2xl">{{ props.data.genre }}</div>
-    <div class="text-3xl">{{ props.data.title }}</div>
-    <div class="text-xl">{{ props.data.artist }}</div>
+    <div class="text-xl">{{ props.data.genre }}</div>
+    <div class="mt-2 text-3xl">{{ props.data.title }}</div>
+    <div class="mt-2 text-xl">{{ props.data.artist }}</div>
+  </div>
+  <iframe
+    width="100%"
+    height="166"
+    scrolling="no"
+    frameborder="no"
+    allow="autoplay"
+    :src="`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${props.data.soundcloud.id}&color=%238f9887&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`"
+  ></iframe>
+  <div
+    style="
+      font-size: 10px;
+      color: #cccccc;
+      line-break: anywhere;
+      word-break: normal;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      font-family:
+        Interstate,
+        Lucida Grande,
+        Lucida Sans Unicode,
+        Lucida Sans,
+        Garuda,
+        Verdana,
+        Tahoma,
+        sans-serif;
+      font-weight: 100;
+    "
+  >
+    <a
+      :href="`https://soundcloud.com/${props.data.soundcloud.user.id}`"
+      :title="props.data.soundcloud.user.userName"
+      target="_blank"
+      style="color: #cccccc; text-decoration: none"
+    >
+      {{ props.data.soundcloud.user.userName }}
+    </a>
+    ·
+    <a
+      :href="props.data.soundcloud.url"
+      title="[BOF:NT] sketch_230831"
+      target="_blank"
+      style="color: #cccccc; text-decoration: none"
+    >
+      {{ props.data.soundcloud.title }}
+    </a>
   </div>
 </template>
--- types/work.ts
+++ types/work.ts
@@ -1,9 +1,19 @@
 export type Work = {
-  title: string,
-  artist: string,
-  genre: string,
+  id: number
+  title: string
+  artist: string
+  genre: string
   bpm: {
-    min: number|null,
-    max: number|null
+    min: number | null
+    max: number | null
+  }
+  soundcloud: {
+    id: number
+    url: string
+    title: string
+    user: {
+      id: string
+      userName: string
+    }
   }
 }
